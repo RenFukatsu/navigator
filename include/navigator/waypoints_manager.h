a9bbb4bd6656f0d7cb9400dd2f49860c4e7ddb14
RenFukatsu <fukatsuren88@gmail.com>
2021-08-01 16:14:29 +0900

    add random walk

--- include/navigator/waypoints_manager.h
+++ include/navigator/waypoints_manager.h
@@ -7,11 +7,14 @@
 #include <std_srvs/SetBool.h>
 #include <std_srvs/Trigger.h>
 
+#include <random>
+
 class WaypointsManager {
  public:
     WaypointsManager();
     void process();
-    void read_waypoints();
+    void read_waypoints(std::vector<geometry_msgs::PoseStamped> *waypoints);
+    void read_points_relation();
     void timer_callback(const ros::TimerEvent &event);
     void rviz_local_goal_callback(const geometry_msgs::PoseStampedConstPtr &pose);
     bool clear_waypoints_service(std_srvs::TriggerRequest &req, std_srvs::TriggerResponse &res);
@@ -29,12 +32,19 @@ class WaypointsManager {
     ros::ServiceClient reached_goal_client_;
     ros::ServiceServer clear_waypoints_server_;
     int HZ;
-    bool WITH_RVIZ;
     bool LOOP_WAYPOINTS;
+    bool RANDOM_WAYPOINTS;
     double GOAL_THRESHOLD;
     geometry_msgs::PoseWithCovarianceStamped current_pose_;
     std::vector<geometry_msgs::PoseStamped> waypoints_;
     bool reached_goal_;
+    // for RAMDOM_WAYPOINTS
+    std::vector<geometry_msgs::PoseStamped> points_;
+    std::vector<std::vector<int>> points_relation_;
+    int pre_pos_idx_;
+    int next_pos_idx_;
+    std::mt19937 mt_;
+    std::uniform_int_distribution<int> dist_;
 };
 
 #endif  // MULTI_ROBOTS_WAYPOINTS_MANAGER_H_
--- src/waypoints_manager.cpp
+++ src/waypoints_manager.cpp
@@ -1,15 +1,27 @@
 #include "navigator/waypoints_manager.h"
 
-WaypointsManager::WaypointsManager() : private_nh_("~"), reached_goal_(false) {
+WaypointsManager::WaypointsManager()
+    : private_nh_("~"), reached_goal_(false), mt_(std::random_device {}()), dist_(0, 1000) {
     pose_sub_ = nh_.subscribe("amcl_pose", 1, &WaypointsManager::pose_callback, this);
     local_goal_pub_ = nh_.advertise<geometry_msgs::PoseStamped>("local_goal", 1);
     reached_goal_client_ = nh_.serviceClient<std_srvs::SetBool>(nh_.getNamespace() + "/reached_goal");
     private_nh_.param("HZ", HZ, 10);
-    private_nh_.param("WITH_RVIZ", WITH_RVIZ, false);
     private_nh_.param("LOOP_WAYPOINTS", LOOP_WAYPOINTS, false);
+    private_nh_.param("RANDOM_WAYPOINTS", RANDOM_WAYPOINTS, false);
     private_nh_.param("GOAL_THRESHOLD", GOAL_THRESHOLD, 0.8);
-    if (!WITH_RVIZ) {
-        read_waypoints();
+    bool WITH_RVIZ;
+    private_nh_.param("WITH_RVIZ", WITH_RVIZ, false);
+    if (RANDOM_WAYPOINTS) {
+        read_waypoints(&points_);
+        read_points_relation();
+        int idx;
+        ROS_ASSERT(private_nh_.getParam("FIRST_POSITION", idx));
+        pre_pos_idx_ = idx;
+        ROS_ASSERT(private_nh_.getParam("FIRST_LOCAL_GOAL", idx));
+        next_pos_idx_ = idx;
+        waypoints_.push_back(points_[next_pos_idx_]);
+    } else if (!WITH_RVIZ) {
+        read_waypoints(&waypoints_);
         ROS_INFO_STREAM("Read waypoints size = " << waypoints_.size() << ".");
     } else {
         local_goal_sub_ = nh_.subscribe("rviz/local_goal", 1, &WaypointsManager::rviz_local_goal_callback, this);
@@ -18,7 +30,7 @@ WaypointsManager::WaypointsManager() : private_nh_("~"), reached_goal_(false) {
     }
 }
 
-void WaypointsManager::read_waypoints() {
+void WaypointsManager::read_waypoints(std::vector<geometry_msgs::PoseStamped> *waypoints) {
     XmlRpc::XmlRpcValue param_list;
     for (size_t i = 0;; i++) {
         std::string param_name = "waypoint" + std::to_string(i);
@@ -40,7 +52,24 @@ void WaypointsManager::read_waypoints() {
         pose_stamped.pose.orientation.y = param_list[5];
         pose_stamped.pose.orientation.z = param_list[6];
 
-        waypoints_.push_back(pose_stamped);
+        waypoints->push_back(pose_stamped);
+    }
+}
+
+void WaypointsManager::read_points_relation() {
+    XmlRpc::XmlRpcValue param_list;
+    for (size_t i = 0;; i++) {
+        std::string param_name = "relation" + std::to_string(i);
+        if (!private_nh_.getParam(param_name.c_str(), param_list)) break;
+        ROS_ASSERT(param_list.getType() == XmlRpc::XmlRpcValue::TypeArray);
+
+        std::vector<int> points;
+        for (size_t j = 0; j < param_list.size(); j++) {
+            ROS_ASSERT(param_list[j].getType() == XmlRpc::XmlRpcValue::TypeInt);
+            points.push_back(param_list[j]);
+        }
+        ROS_ASSERT(points_relation_.size() == i);
+        points_relation_.push_back(points);
     }
 }
 
@@ -49,8 +78,7 @@ void WaypointsManager::rviz_local_goal_callback(const geometry_msgs::PoseStamped
     ROS_INFO_STREAM("Add waypoints. The Number of waypoints is " << waypoints_.size() << ".");
 }
 
-bool WaypointsManager::clear_waypoints_service(std_srvs::TriggerRequest &req,
-                                               std_srvs::TriggerResponse &res) {
+bool WaypointsManager::clear_waypoints_service(std_srvs::TriggerRequest &req, std_srvs::TriggerResponse &res) {
     waypoints_.clear();
     res.success = true;
     res.message = "Clear waypoints.";
@@ -72,9 +100,19 @@ void WaypointsManager::timer_callback(const ros::TimerEvent &event) {
     if (way_points_idx < waypoints_.size() && is_close_local_goal(current_pose_, waypoints_[way_points_idx])) {
         way_points_idx++;
     }
-    if (way_points_idx >= waypoints_.size() && LOOP_WAYPOINTS) {
+    if (way_points_idx == waypoints_.size() && RANDOM_WAYPOINTS) {
+        const auto &candidate = points_relation_[next_pos_idx_];
+        int random_idx = dist_(mt_) % candidate.size();
+        if (candidate[random_idx] == pre_pos_idx_) {
+            random_idx++;
+            random_idx %= candidate.size();
+        }
+        pre_pos_idx_ = next_pos_idx_;
+        next_pos_idx_ = candidate[random_idx];
+        waypoints_.push_back(points_[next_pos_idx_]);
+    } else if (way_points_idx == waypoints_.size() && LOOP_WAYPOINTS) {
         way_points_idx = 0;
-    } else if (way_points_idx >= waypoints_.size()) {
+    } else if (way_points_idx >= waypoints_.size()) {  // >= for when clear_waypoints is called
         if (way_points_idx != waypoints_.size()) way_points_idx = waypoints_.size();
         ROS_INFO_THROTTLE(15.0, "Robot reached last waypoint.");
         if (!reached_goal_) {
